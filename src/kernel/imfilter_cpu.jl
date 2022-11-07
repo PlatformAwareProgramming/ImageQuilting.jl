509bd1c36a96f2ac7afce501d4abb89af64fbff9
Francisco Heron de Carvalho Junior <heron@dc.ufc.br>
2022-11-07 11:22:07 -0500

    ...

---
+++ src/kernel/imfilter_cpu.jl
@@ -0,0 +1,19 @@
+# ------------------------------------------------------------------
+# Licensed under the MIT License. See LICENCE in the project root.
+# ------------------------------------------------------------------
+
+function init_kernel(::CPUMethod) 
+  @info "Running default kernel"
+end
+
+const array_kernel(::CPUMethod, array) = array
+
+const view_kernel(::CPUMethod, array, I) = view(array, I)
+
+function imfilter_kernel(::CPUMethod, img, krn)
+  imfilter_cpu(img, krn)
+end
+
+function imfilter_cpu(img, krn)
+  imfilter(img, centered(krn), Inner(), Algorithm.FFT())
+end
