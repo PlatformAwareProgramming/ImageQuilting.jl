feff8b7ac567ee248e90bf622b24563f9ec65771
Francisco Heron de Carvalho Junior <heron@dc.ufc.br>
2022-11-07 16:25:30 -0500

    structured version

---
+++ src/kernel/kernel_features.jl
@@ -0,0 +1,7 @@
+# ------------------------------------------------------------------
+# Licensed under the MIT License. See LICENCE in the project root.
+# ------------------------------------------------------------------
+@platform parameter clear
+@platform parameter accelerator_count
+@platform parameter accelerator_api
+@platform parameter accelerator_manufacturer
