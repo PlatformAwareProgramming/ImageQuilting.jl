9fb35664f54e89fea47602d64eedd698d7b2a2b5
Francisco Heron de Carvalho Junior <heron@dc.ufc.br>
2022-11-07 11:25:29 -0500

    ...

--- run_experiment_app.sh
+++ run_experiment_app.sh
@@ -3,9 +3,9 @@
 for turn in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35
 do
    echo $turn.adhoc
-   git switch iq-sim-refact
+   git switch iq-sim-refact-adhoc
    PATH_IQ="." $JULIA_PATH/julia ./run_sample_app.jl $turn >> output.iq.app.adhoc
    echo $turn.structured
-   git switch iq-sim-refact-adhoc
+   git switch iq-sim-refact
    PATH_IQ="." $JULIA_PATH/julia ./run_sample_app.jl $turn >> output.iq.app.structured
 done
