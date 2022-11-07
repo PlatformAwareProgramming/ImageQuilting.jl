bbe35222e3ef1b76b51270e983c2a302d9a4d706
Francisco Heron de Carvalho Junior <heron@dc.ufc.br>
2022-11-07 16:24:51 -0500

    structured version

---
+++ src/imfilter.jl
@@ -0,0 +1,8 @@
+# ------------------------------------------------------------------
+# Licensed under the MIT License. See LICENCE in the project root.
+# 
+  
+include("kernel/kernel_features.jl")
+include("kernel/imfilter_default.jl")
+include("kernel/imfilter_cuda.jl")
+include("kernel/imfilter_opencl.jl")
