46921457734ef99e7d21aa0c854886c2cd9f53b4
Francisco Heron de Carvalho Junior <heron@dc.ufc.br>
2022-11-08 17:59:31 -0500

    ...

---
+++ src/imfilter.jl
@@ -0,0 +1,45 @@
+# ------------------------------------------------------------------
+# Licensed under the MIT License. See LICENCE in the project root.
+# ------------------------------------------------------------------
+
+include("kernel/imfilter_cpu.jl")
+include("kernel/imfilter_cuda.jl")
+include("kernel/imfilter_opencl.jl")
+
+function opencl_functional()
+  try
+    !isempty(cl.devices())
+  catch err
+    if err isa cl.CLError
+      false
+    else
+      rethrow(err)
+    end
+  end
+end
+
+function cuda_functional()
+  try 
+    CUDA.functional()
+  catch 
+    false
+  end
+end
+
+function select_kernel_method()
+
+  if cuda_functional()
+    CUDAMethod()
+  elseif opencl_functional()
+    OpenCLMethod()
+  else
+    CPUMethod()
+  end
+end
+
+const kernel_method = select_kernel_method()
+
+const init_kernel() = init_kernel(kernel_method)
+const array_kernel(array) = array_kernel(kernel_method, array)
+const view_kernel(array, I) = view_kernel(kernel_method, array, I)
+const imfilter_kernel(img, krn) = imfilter_kernel(kernel_method, img, krn)
