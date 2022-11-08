b1823bab36c1e10fae1180e2283933820f017851
Francisco Heron de Carvalho Junior <heron@dc.ufc.br>
2022-11-08 17:52:22 -0500

    ...

--- src/kernels.jl
+++
@@ -1,7 +0,0 @@
-# ------------------------------------------------------------------
-# Licensed under the MIT License. See LICENCE in the project root.
-# ------------------------------------------------------------------
-@platform parameter clear
-@platform parameter accelerator_count
-@platform parameter accelerator_api
-@platform parameter accelerator_manufacturer
